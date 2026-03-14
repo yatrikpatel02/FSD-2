25e069d7ffbae0381d7c00670d72643f1e06adf6
yatrikpatel02 <yatrikpatel02@gmail.com>
2026-03-14 13:00:36 +0530

    Add files via upload

---
+++ node/task.js
@@ -0,0 +1,11 @@
+fs = require("fs")
+//fs.mkdirSync("fs")
+fs.writeFileSync("fs/my.txt", "B2 Students")
+fs.appendFileSync("fs/my.txt", "\n Node JS Tutorial")
+r = fs.readFileSync("fs/my.txt", "UTF-8")
+fs.copyFileSync("fs/my.txt", "fs/mynew.txt")        //directly copy the file
+//fs.writeFileSync("fs/mynew.txt", "B2 Students")
+//fs.appendFileSync("fs/mynew.txt", r)
+r1 = fs.readFileSync("fs/mynew.txt", "UTF-8")
+fs.unlinkSync("fs/my.txt")
+console.log(r1)
