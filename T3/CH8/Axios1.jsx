43c40cb4552d94e9316ab46bb8b9f6e200ecebfb
yatrikpatel02 <yatrikpatel02@gmail.com>
2026-06-04 13:19:27 +0530

    Add files via upload

--- T3/CH8/Axios1.jsx
+++ T3/CH8/Axios1.jsx
@@ -22,3 +22,7 @@ export default function Axios1() {
         </>
     )
 }
+
+// for practice 1) - https://official-joke-api.appspot.com/ramdom_joke
+// for practice 2) - https://dog.ceo/api/breeds/image/random
+// for practice 3) - https://dummy.json.com/quotes
